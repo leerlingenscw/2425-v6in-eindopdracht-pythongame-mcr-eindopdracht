b89d4fd5a00e4e826ae9ed21799d6cef2b2a0ce2
CharlotteVerdonck <charcharverdonck@gmail.com>
2025-03-09 17:44:37 +0000

    1000 agents

--- main.py
+++ main.py
@@ -407,7 +407,7 @@ class GeneticAlgorithm:
 
 #---------------------------Loop---------------------------------
 if __name__ == "__main__":
-    population_size = 500
+    population_size = 1000
     mutation_rate = 0.05
     generations = 1000000
     state_size = 23 # Number of inputs
